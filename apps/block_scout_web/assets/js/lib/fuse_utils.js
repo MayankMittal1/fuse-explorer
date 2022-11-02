51bdca4d178184d99b413ace730c13bf0d96f4e4
Mayank Mittal <msquare.pvt21@gmail.com>
2022-11-02 15:44:39 +0530

    UI: Dark theme Fixes

--- apps/block_scout_web/assets/js/lib/fuse_utils.js
+++ apps/block_scout_web/assets/js/lib/fuse_utils.js
@@ -1,34 +1,6 @@
 import { of } from 'rxjs'
 import { delay, tap, mergeMap, repeat } from 'rxjs/operators'
-import $ from 'jquery'
 import 'jquery-circle-progress'
-
-class ProgressCircle {
-  constructor ($el) {
-    this.$el = $el
-    this.init()
-  }
-
-  init () {
-    $(this.$el).circleProgress({
-      value: 0,
-      size: 150,
-      thickness: 12,
-      emptyFill: '#49687c',
-      lineCap: 'round'
-    }).on('circle-animation-progress', function (event, progress, stepValue) {
-      $(this).find('.progress-value').html(`${Math.trunc(stepValue * 100)}%`)
-    })
-  }
-
-  set (value) {
-    $(this.$el).circleProgress('value', value)
-  }
-}
-
-export function createCycleEndProgressCircle ($el) {
-  return new ProgressCircle($el)
-}
 
 export function secondsToDhms (seconds) {
   seconds = Number(seconds)
